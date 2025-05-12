2457809ae318555d3fc8b64c13325c9bc950a425
Hasan Akgun <akgunhasan8@gmail.com>
2025-05-12 23:03:11 +0300

    fetch data from backend

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -3,6 +3,14 @@ import { products } from '../../starting-code/data/products'
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+    })
+  
+
   return (
     <>
       <title>Ecommerce Project</title>
@@ -26,14 +34,14 @@ export function HomePage() {
 
                 <div className="product-rating-container">
                   <img className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars*10}.png`} />
+                    src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                   <div className="product-rating-count link-primary">
                     {product.rating.count}
                   </div>
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents/100).toFixed(2)}
+                  ${(product.priceCents / 100).toFixed(2)}
                 </div>
 
                 <div className="product-quantity-container">
